59fc07d0f0ec556f0dcc7cc4663875deab77ba33
czewrony42 <czewrony42@gmail.com>
2023-02-25 13:34:42 +0100

    dodalem modul ciasteczek

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const fortune = require("./lib/fortune");
 
-const fortunes = [
-    "Każda wrózba kończy się śmiercią",
-    "Co cie nie zabije to Cie pokaleczy",
-    "Nie wchodzi się dwa razy do tego samego grobu",
-    "Jebać PIS",
-    "PO też kradło!"
-];
-
-const app = express()
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 // konfiguracja silnika widoków Handlebars
-app.engine('handlebars', expressHandlebars.engine())
-app.set('view engine', 'handlebars')
+app.engine('handlebars', expressHandlebars.engine());
+app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => res.render('home'));
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 })
 
 // niestandardowa strona 404
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+    res.status(404);
+    res.render('404');
 })
 
 // niestandardowa strona 500
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
 })
 
 app.listen(port, () => console.log(
